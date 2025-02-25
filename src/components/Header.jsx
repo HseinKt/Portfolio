62ac3aabbef177b099e8cb033eb17faff1da41d6
HseinKt <hseinkteish1998@gmail.com>
2025-02-26 00:54:18 +0200

    Finished Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,19 @@ import { useState } from "react";
 import '../CSS/Header.css'
 import { AiOutlineFilePdf } from "react-icons/ai";
 import { FiMenu } from "react-icons/fi";
+import { motion } from "framer-motion"
 
 const Header = () => {
     const [active, setActive] = useState("Home");
+    const [isOpen, setIsOpen] = useState(false);
 
     const handleClick = (section) => {
-        console.log("Clicked:", section); 
         setActive(section);
+    }
+
+    const handleMenu = (section) => {
+        setActive(section);
+        setIsOpen(!isOpen);
     }
 
     // const cvURL = "https://github.com/HseinKt/Portfolio/raw/main/public/Hussein%20Kteish_Resume.pdf";
@@ -19,6 +25,7 @@ const Header = () => {
             <div className="nav-logo">
                 <p className="nav-name">Hussein Kteish</p>
             </div>
+
             <div className="nav-menu">
                 <ul>
                     {["Home", "About", "Projects", "Experience", "Contact"].map((section) => (
@@ -33,13 +40,41 @@ const Header = () => {
                     ))}
                 </ul>
             </div>
+
             <div className="nav-btn">
                 <a href={cvURL} download>
                     <button className="btn">Download CV <AiOutlineFilePdf /> </button>
                 </a>
             </div>
-            <div className="nav-menu-btn">
-                <FiMenu className="fimenu" />
+
+            <div className="nav-menu-dropdown">
+                <a 
+                    className="menu-a"
+                    onClick={() => setIsOpen(!isOpen)}
+                >
+                    <FiMenu className="fimenu" />
+                </a>
+                
+                { isOpen && <div className="dropdown-menu">
+                    <motion.ul 
+                        className="menu-list"
+                        initial={{ x: "100%" }} // Start off-screen to the right
+                        animate={{ x: isOpen ? "0%" : "100%" }} // Slide in/out
+                        transition={{ duration: 0.4, ease: "easeInOut" }}
+                    >
+                        {["Home", "About", "Projects", "Experience", "Contact"].map((section) => (
+                            <li key={section}>
+                                <a 
+                                    href={`#${section.toLowerCase()}`} 
+                                    className={`nav-link ${active === section? "active-link" : ""}`} 
+                                    onClick={() => handleMenu(section)}>
+                                        {section}
+                                </a>
+                            </li>
+                        ))}
+                    </motion.ul>
+                </div>
+                }
             </div>
         </div>
      );
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,6 +2,52 @@ const Contact = () => {
     return ( 
         <>
         <h2>Contact</h2>
+
+        <h1>Hello, I'm a React Developer! </h1>
+            <p>Intro </p>
+            <button>Hire Me</button>
+            image
+            linkedIN and GitHub
+            <h1>Hello, I'm a React Developer! </h1>
+            <p>Intro </p>
+            <button>Hire Me</button>
+            image
+            linkedIN and GitHub
+            <h1>Hello, I'm a React Developer! </h1>
+            <p>Intro </p>
+            <button>Hire Me</button>
+            image
+            linkedIN and GitHub
+            <h1>Hello, I'm a React Developer! </h1>
+            <p>Intro </p>
+            <button>Hire Me</button>
+            image
+            linkedIN and GitHub
+            <h1>Hello, I'm a React Developer! </h1>
+            <p>Intro </p>
+            <button>Hire Me</button>
+            image
+            linkedIN and GitHub
+            <h1>Hello, I'm a React Developer! </h1>
+            <p>Intro </p>
+            <button>Hire Me</button>
+            image
+            linkedIN and GitHub
+            <h1>Hello, I'm a React Developer! </h1>
+            <p>Intro </p>
+            <button>Hire Me</button>
+            image
+            linkedIN and GitHub
+            <h1>Hello, I'm a React Developer! </h1>
+            <p>Intro </p>
+            <button>Hire Me</button>
+            image
+            linkedIN and GitHub
+            <h1>Hello, I'm a React Developer! </h1>
+            <p>Intro </p>
+            <button>Hire Me</button>
+            image
+            linkedIN and GitHub
         </>
      );
 }
