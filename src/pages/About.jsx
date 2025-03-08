b94abed8801591836fa2076bd3187d5c04a9d175
HseinKt <hseinkteish1998@gmail.com>
2025-03-08 17:41:20 +0200

    Finished skills option on About Me section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -21,8 +21,41 @@ const About = () => {
                         </div>
                     </div>
                 </div>
-                <div className="col">
-                    Skills
+                <div className="col skill-col">
+                    <div className="skills-box">
+                        <div className="skills-header">
+                            <h3>Frontend</h3>
+                        </div>
+                        <div className="skills-list">
+                            <span>HTML5</span>
+                            <span>CSS3</span>
+                            <span>JavaScript</span>                    
+                            <span>React</span>
+                            <span>UI/UX Design</span>
+                        </div>
+                    </div>
+
+                    <div className="skills-box">
+                        <div className="skills-header">
+                            <h3>Backend</h3>
+                        </div>
+                        <div className="skills-list">
+                            <span>Laravel (PHP)</span>
+                            <span>RESTFUL APIs</span>
+                            <span>MySQL</span>                    
+                        </div>
+                    </div>
+
+                    <div className="skills-box">
+                        <div className="skills-header">
+                            <h3>Tools</h3>                        </div>
+                        <div className="skills-list">
+                            <span>Git</span>
+                            <span>GitHub</span>                    
+                            <span>Postman</span>
+                            <span>Figma</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
@@ -30,3 +63,26 @@ const About = () => {
 }
  
 export default About;
+
+
+{/* <h3>Skills & Experiences</h3>
+<ul>
+    <li>React.js</li>
+    <li>JavaScript</li>
+    <li>UI/UX Design</li>
+    <li>HTML5</li>
+    <li>CSS3</li>
+    <li>SASS</li>
+    <li>Bootstrap</li>
+    <li>Responsive Design</li>
+    <li>Git</li>
+    <li>Version Control</li>
+    <li>Agile Methodologies</li>
+    <li>Software Development</li>
+    <li>Project Management</li>
+</ul>
+ <h3>Education</h3>
+<ul>
+    <li>BSc Computer Science, University of Jordan</li>
+    <li>Certified Scrum Master, Scrum Alliance</li>
+</ul>  */}
