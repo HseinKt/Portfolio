ad6921dde21be00a012211027730f62f97919c86
HseinKt <hseinkteish1998@gmail.com>
2025-06-02 15:54:42 +0300

    Updated

--- src/components/DownloadCV.jsx
+++ src/components/DownloadCV.jsx
@@ -1,11 +1,11 @@
 import { AiOutlineFilePdf } from "react-icons/ai";
 
 const DownloadCV = () => {
-    const cvURL = "https://drive.google.com/file/d/1R97wyrkZt8CAPu7NG-vPY28Pv_oVjoYU/view?usp=sharing"
+    const cvURL = "https://drive.google.com/file/d/1xdsMviM2clVtmq4c_no8NrK12pQ4j9zt/view?usp=sharing"
 
     return ( 
         <>
-            <a href={cvURL} download="Hussein_Kteish_Resume.pdf">
+            <a href={cvURL} download="Hussein_Kteish_Resume.pdf" target="_blank" rel="noopener noreferrer">
                 <button className="btn">Download CV <AiOutlineFilePdf /> </button>
             </a>
         </>
